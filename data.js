01b207eb891cf8d528acdb59840e15655362bfd8
Sandeep Jha <sandeepjha@Sandeeps-MacBook-Pro.local>
2025-01-17 00:14:18 +0530

    minor changes

--- data.js
+++ data.js
@@ -1,34 +1,38 @@
 async function fetchTimetable() {
-  const sapId = document.getElementById('SapId').value;
-  const response = await fetch('combined_data.json');
-  const data = await response.json();
+    const sapId = document.getElementById('SapId').value;
+    const response = await fetch('combined_data.json');
+    const data = await response.json();
 
-  const student = data[sapId];
-  if (student) {
-    displayTimetable(student.timeTable);
-  } else {
-    document.getElementById('timetable').innerHTML = 'No timetable found for this SAP ID.';
-  }
+    const student = data[sapId];
+    if (student) {
+        displayTimetable(student.timeTable);
+    } else {
+        document.getElementById('timetable').innerHTML = 'No timetable found for this SAP ID.';
+    }
 }
 
 function displayTimetable(timeTable) {
-  const timetableDiv = document.getElementById('timetable');
-  timetableDiv.innerHTML = '';
+    const timetableDiv = document.getElementById('timetable');
+    timetableDiv.innerHTML = '';
 
-  for (const day in timeTable) {
-    const dayDiv = document.createElement('div');
-    dayDiv.innerHTML = `<h2>${day}</h2>`;
-    
-    timeTable[day].forEach(classInfo => {
-      const classDiv = document.createElement('div');
-      classDiv.innerHTML = `
-        <p>Class: ${classInfo.class}</p>
-        <p>Time: ${classInfo.time}</p>
-        <p>Room: ${classInfo.location}</p>
+    for (const day in timeTable) {
+        const dayDiv = document.createElement('div');
+        dayDiv.innerHTML = `
+        <div style="margin: 5vh 50vh; border: 1px dotted black;">
+        <h2>${day}</h2>
+        </div>
+        `;
+
+        timeTable[day].forEach(classInfo => {
+            const classDiv = document.createElement('div');
+            classDiv.innerHTML = `
+            <p>Class: ${classInfo.class}</p>
+            <p>Time: ${classInfo.time}</p>
+            <p>Room: ${classInfo.location}</p>
       `;
-      dayDiv.appendChild(classDiv);
-    });
+            dayDiv.appendChild(classDiv);
+        });
 
-    timetableDiv.appendChild(dayDiv);
-  }
+        timetableDiv.appendChild(dayDiv);
+    }
 }
